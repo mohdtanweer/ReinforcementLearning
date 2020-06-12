8c5cf7a1c2abab12ca8c3ccb71db842dbcafc24d
Mohammad Tanweer <mohammadtanweer@Mohammads-MacBook-Pro.local>
2020-06-12 16:24:04 +0100

    Initial commit

--- comparing_epsilons.py
+++ comparing_epsilons.py
@@ -1,70 +1,84 @@
+from __future__ import print_function, division
+from builtins import range
+# Note: you may need to update your version of future
+# sudo pip install -U future
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class Bandit:
-    def __init__(self, m):
-        self.m = m
-        self.mean = 0
-        self.N = 0
+  def __init__(self, m):
+    self.m = m
+    self.mean = 0
+    self.N = 0
 
-    def pull(self):
-        return np.random.randn() + self.m
+  def pull(self):
+    return np.random.randn() + self.m
 
-    def update(self, x):
-        self.N += 1
-        self.mean = (1 - 1.0/self.N)*self.mean + 1.0/self.N * x
+  def update(self, x):
+    self.N += 1
+    self.mean = (1 - 1.0/self.N)*self.mean + 1.0/self.N*x
 
 
 def run_experiment(m1, m2, m3, eps, N):
-    bandits = [Bandit(m1), Bandit(m2), Bandit(m3)]
+  bandits = [Bandit(m1), Bandit(m2), Bandit(m3)]
 
-    data = np.empty(N)
+  # Count no of suboptimal choice
+  means = np.array([m1, m2, m3])
+  true_best = np.argmax(means)
+  count_suboptimal = 0
 
+  data = np.empty(N)
+  
+  for i in range(N):
     # epsilon greedy
-    for i in range(N):
-        p = np.random.random()
-        if p < eps:
-            j = np.random.choice(3)
-        else:
-            j = np.argmax([b.mean for b in bandits])
+    p = np.random.random()
+    if p < eps:
+      j = np.random.choice(3)
+    else:
+      j = np.argmax([b.mean for b in bandits])
+    x = bandits[j].pull()
+    bandits[j].update(x)
+    if j != true_best:
+      count_suboptimal += 1
 
-        x = bandits[j].pull()
-        bandits[j].update(x)
+    # for the plot
+    data[i] = x
+  cumulative_average = np.cumsum(data) / (np.arange(N) + 1)
 
-        # data for the plot
-        data[i] = x
+  # plot moving average ctr
+  plt.plot(cumulative_average)
+  plt.plot(np.ones(N)*m1)
+  plt.plot(np.ones(N)*m2)
+  plt.plot(np.ones(N)*m3)
+  plt.xscale('log')
+  plt.show()
 
-    cumulative_average = np.cumsum(data) / (np.arange(N) + 1)
+  for b in bandits:
+    print(b.mean)
 
-    # Plot moving average ctr
-    plt.plot(cumulative_average)
-    plt.plot(np.ones(N) * m1)
-    plt.plot(np.ones(N) * m2)
-    plt.plot(np.ones(N) * m3)
-    plt.xscale('log')
-    plt.show()
+  print("percent suboptimal for epsilon = %s" % eps, float(count_suboptimal) / N)
 
-    for b in bandits:
-        print (b.mean)
-
-    return cumulative_average
+  return cumulative_average
 
 if __name__ == '__main__':
-    c_1 = run_experiment(1.0, 2.0, 3.0, 0.1, 100000)
-    c_05 = run_experiment(1.0, 2.0, 3.0, 0.05, 100000)
-    c_01 = run_experiment(1.0, 2.0, 3.0, 0.01, 100000)
+  c_1 = run_experiment(1.0, 2.0, 3.0, 0.1, 100000)
+  c_05 = run_experiment(1.0, 2.0, 3.0, 0.05, 100000)
+  c_01 = run_experiment(1.0, 2.0, 3.0, 0.01, 100000)
 
-    # log scale plot
-    plt.plot(c_1, label='eps = 0.1')
-    plt.plot(c_05, label='eps = 0.05')
-    plt.plot(c_01, label='eps = 0.01')
-    plt.legend()
-    plt.xscale('log')
-    plt.show()
+  # log scale plot
+  plt.plot(c_1, label='eps = 0.1')
+  plt.plot(c_05, label='eps = 0.05')
+  plt.plot(c_01, label='eps = 0.01')
+  plt.legend()
+  plt.xscale('log')
+  plt.show()
 
-    # linear plot
-    plt.plot(c_1, label='eps = 0.1')
-    plt.plot(c_05, label='eps = 0.05')
-    plt.plot(c_01, label='eps = 0.01')
-    plt.legend()
-    plt.show()
+
+  # linear plot
+  plt.plot(c_1, label='eps = 0.1')
+  plt.plot(c_05, label='eps = 0.05')
+  plt.plot(c_01, label='eps = 0.01')
+  plt.legend()
+  plt.show()
--- optimistic_initial_value_theory/optimistic_initial_values.py
+++ optimistic_initial_value_theory/optimistic_initial_values.py
@@ -3,66 +3,67 @@ from builtins import range
 # Note: you may need to update your version of future
 # sudo pip install -U future
 
-
-import matplotlib.pyplot as plt
 import numpy as np
-
-
-NUM_TRIALS = 10000
-EPS = 0.1
-BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]
+import matplotlib.pyplot as plt
+from comparing_epsilons import run_experiment as run_experiment_eps
 
 
 class Bandit:
-  def __init__(self, p, upper_limit):
-    # p: the win rate
-    self.p = p
-    self.p_estimate = upper_limit # TODO
-    self.N = 1 # TODO
+  def __init__(self, m, upper_limit):
+    self.m = m
+    self.mean = upper_limit
+    self.N = 1
 
   def pull(self):
-    # draw a 1 with probability p
-    return np.random.random() < self.p
+    return np.random.randn() + self.m
 
   def update(self, x):
-    # TODO
-    self.p_estimate = # TODO
+    self.N += 1
+    self.mean = (1 - 1.0/self.N)*self.mean + 1.0/self.N*x
 
 
-def experiment():
-  bandits = [Bandit(p) for p in BANDIT_PROBABILITIES]
+def run_experiment(m1, m2, m3, N, upper_limit=10):
+  bandits = [Bandit(m1, upper_limit), Bandit(m2, upper_limit), Bandit(m3, upper_limit)]
 
-  rewards = np.zeros(NUM_TRIALS)
-  for i in range(NUM_TRIALS):
-    # use optimistic initial values to select the next bandit
-    j = # TODO
-
-    # pull the arm for the bandit with the largest sample
+  data = np.empty(N)
+  
+  for i in range(N):
+    # optimistic initial values
+    j = np.argmax([b.mean for b in bandits])
     x = bandits[j].pull()
-
-    # update rewards log
-    rewards[i] = x
-
-    # update the distribution for the bandit whose arm we just pulled
     bandits[j].update(x)
 
+    # for the plot
+    data[i] = x
+  cumulative_average = np.cumsum(data) / (np.arange(N) + 1)
 
-  # print mean estimates for each bandit
-  for b in bandits:
-    print("mean estimate:", b.p_estimate)
-
-  # print total reward
-  print("total reward earned:", rewards.sum())
-  print("overall win rate:", rewards.sum() / NUM_TRIALS)
-  print("num times selected each bandit:", [b.N for b in bandits])
-
-  # plot the results
-  cumulative_rewards = np.cumsum(rewards)
-  win_rates = cumulative_rewards / (np.arange(NUM_TRIALS) + 1)
-  plt.ylim([0, 1])
-  plt.plot(win_rates)
-  plt.plot(np.ones(NUM_TRIALS)*np.max(BANDIT_PROBABILITIES))
+  # plot moving average ctr
+  plt.plot(cumulative_average)
+  plt.plot(np.ones(N)*m1)
+  plt.plot(np.ones(N)*m2)
+  plt.plot(np.ones(N)*m3)
+  plt.xscale('log')
   plt.show()
 
-if __name__ == "__main__":
-  experiment()
+  for b in bandits:
+    print(b.mean)
+
+  return cumulative_average
+
+if __name__ == '__main__':
+  c_1 = run_experiment_eps(1.0, 2.0, 3.0, 0.1, 100000)
+  oiv = run_experiment(1.0, 2.0, 3.0, 100000)
+
+  # log scale plot
+  plt.plot(c_1, label='eps = 0.1')
+  plt.plot(oiv, label='optimistic')
+  plt.legend()
+  plt.xscale('log')
+  plt.show()
+
+
+  # linear plot
+  plt.plot(c_1, label='eps = 0.1')
+  plt.plot(oiv, label='optimistic')
+  plt.legend()
+  plt.show()
